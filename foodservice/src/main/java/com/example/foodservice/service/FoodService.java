105f1a0b81bb1d28ff064ddf41149145677c60a0
Dean Yim <donghyeon0226@gmail.com>
2024-12-12 11:12:46 -0800

    dddd

--- foodservice/src/main/java/com/example/foodservice/service/FoodService.java
+++ foodservice/src/main/java/com/example/foodservice/service/FoodService.java
@@ -1,7 +1,12 @@
 package com.example.foodservice.service;
 
+import com.example.foodservice.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FoodService {
+
+    @Autowired
+    FoodRepository foodRepository;
 }
